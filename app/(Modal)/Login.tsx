c2214e970aaf49465a597018967dcd10e9bf887f
yoshiro1786 <66931868+ymirandav@users.noreply.github.com>
2023-11-27 08:57:49 -0500

    Location

--- app/(Modal)/Login.tsx
+++ app/(Modal)/Login.tsx
@@ -79,4 +79,3 @@ const styles = StyleSheet.create({
 });
 
 export default Login;
-+
--- app/(Modal)/location-search.tsx
+++ app/(Modal)/location-search.tsx
@@ -1,62 +1,154 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import React, { useState } from 'react';
-import MapView from 'react-native-maps';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+import React, { useState, useRef } from 'react';
+import MapView, { Marker, Polyline } from 'react-native-maps';
 import Colors from '@/constants/Colors';
 import { useNavigation } from 'expo-router';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GooglePlaceData, GooglePlaceDetail } from 'react-native-google-places-autocomplete';
 import { Ionicons } from '@expo/vector-icons';
 
 const LocationSearch = () => {
   const navigation = useNavigation();
-  const [location, setLocation] = useState({
+  const [origin, setOrigin] = useState({
     latitude: -16.423399904631825,
-    longitude:  -71.55657756931967,
+    longitude: -71.55657756931967,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+  const [destination, setDestination] = useState({
+    latitude: -16.423399904631825,
+    longitude: -71.55657756931967,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   });
 
+  const mapRef = useRef<MapView | null>(null);
+
+  const [busRoutes, setBusRoutes] = useState([
+    { id: '1', name: 'Bus 101', origin: 'Terminal A', destination: 'Station X' },
+    { id: '2', name: 'Bus 202', origin: 'Station B', destination: 'Station Y' },
+    // Agrega más rutas de autobús según sea necesario
+  ]);
+
+  const handleOriginSelect = (data: GooglePlaceData, details: GooglePlaceDetail | null) => {
+    if (details && details.geometry && details.geometry.location) {
+      const { lat, lng } = details.geometry.location;
+      setOrigin({
+        latitude: lat,
+        longitude: lng,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+      zoomToLocation(lat, lng);
+    }
+  };
+
+  const handleDestinationSelect = (data: GooglePlaceData, details: GooglePlaceDetail | null) => {
+    if (details && details.geometry && details.geometry.location) {
+      const { lat, lng } = details.geometry.location;
+      setDestination({
+        latitude: lat,
+        longitude: lng,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+      zoomToLocation(lat, lng);
+    }
+  };
+
+  const zoomToLocation = (latitude: number, longitude: number) => {
+    if (mapRef.current) {
+      mapRef.current.animateToRegion({
+        latitude,
+        longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    }
+  };
+
   return (
     <View style={{ flex: 1 }}>
-      <GooglePlacesAutocomplete
-        placeholder="Search or move the map"
-        fetchDetails={true}
-        onPress={(data, details) => {
-          const point = details?.geometry?.location;
-          if (!point) return;
-          setLocation({
-            ...location,
-            latitude: point.lat,
-            longitude: point.lng,
-          });
-        }}
-        query={{
-          key: process.env.EXPO_PUBLIC_GOOGLE_API_KEY,
-          language: 'en',
-        }}
-        renderLeftButton={() => (
-          <View style={styles.boxIcon}>
-            <Ionicons name="search-outline" size={24} color={Colors.medium} />
-          </View>
-        )}
-        styles={{
-          container: {
-            flex: 0,
-          },
-          textInput: {
-            backgroundColor: Colors.grey,
-            paddingLeft: 35,
-            borderRadius: 10,
-          },
-          textInputContainer: {
-            padding: 8,
-            backgroundColor: '#fff',
-          },
-        }}
-      />
-      <MapView showsUserLocation={true} style={styles.map} region={location} />
+      <View style={styles.searchContainer}>
+        <View style={styles.searchBox}>
+          <GooglePlacesAutocomplete
+            placeholder='Origen'
+            styles={{
+              container: {
+                flex: 0,
+              },
+              textInput: {
+                fontSize: 18,
+              },
+            }}
+            onPress={handleOriginSelect}
+            fetchDetails={true}
+            enablePoweredByContainer={false}
+            minLength={2}
+            query={{
+              key: process.env.EXPO_PUBLIC_GOOGLE_API_KEY,
+              language: 'es',
+            }}
+            nearbyPlacesAPI='GooglePlacesSearch'
+            debounce={400}
+          />
+        </View>
+        <View style={styles.searchBox}>
+          <GooglePlacesAutocomplete
+            placeholder='Destino'
+            styles={{
+              container: {
+                flex: 0,
+              },
+              textInput: {
+                fontSize: 18,
+              },
+            }}
+            onPress={handleDestinationSelect}
+            fetchDetails={true}
+            enablePoweredByContainer={false}
+            minLength={2}
+            query={{
+              key: process.env.EXPO_PUBLIC_GOOGLE_API_KEY,
+              language: 'es',
+            }}
+            nearbyPlacesAPI='GooglePlacesSearch'
+            debounce={400}
+          />
+        </View>
+      </View>
+
+      <MapView
+        ref={(map) => (mapRef.current = map)}
+        showsUserLocation={true}
+        style={styles.map}
+        region={origin}>
+        <Marker coordinate={origin} pinColor="blue" />
+        <Marker coordinate={destination} pinColor="red" />
+        <Polyline
+          coordinates={[origin, destination]}
+          strokeColor="green"
+          strokeWidth={4}
+        />
+      </MapView>
+
+      <View style={styles.busRoutesContainer}>
+        <Text style={styles.busRoutesTitle}>Rutas de Autobús</Text>
+        <FlatList
+          data={busRoutes}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View style={styles.busRouteItem}>
+              <Text style={styles.busRouteName}>{item.name}</Text>
+              <Text>{`${item.origin} - ${item.destination}`}</Text>
+            </View>
+          )}
+        />
+      </View>
+
       <View style={styles.absoluteBox}>
         <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
-          <Text style={styles.buttonText}>Confirm</Text>
+          <Text style={styles.buttonText}>Confirmar</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -84,11 +176,33 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 16,
   },
-  boxIcon: {
-    position: 'absolute',
-    left: 15,
-    top: 18,
-    zIndex: 1,
+  searchContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 16,
+    marginTop: 8,
+  },
+  searchBox: {
+    flex: 1,
+    marginRight: 8,
+  },
+  busRoutesContainer: {
+    flex: 1,
+    padding: 16,
+    borderTopWidth: 1,
+    borderTopColor: '#ccc',
+  },
+  busRoutesTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  busRouteItem: {
+    marginBottom: 8,
+  },
+  busRouteName: {
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
